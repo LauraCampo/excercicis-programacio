fcd2eb755dc3c0d9bcf5b5dcf6ee312048c5740c
Laura <lauracampodeveloper@gmail.com>
2016-12-22 15:30:50 +0100

    Array con índices
    Crear un array con índices

---
+++ ArrayconIndices.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>ARRAY CON INDICES</title>
+</head>
+<body>
+    <?php
+        $marray = array(0=>100,"color"=>"red");      
+        echo($marray[0]);
+        echo("<br>");
+        echo($marray["color"]);
+    ?>
+</body>
+</html>
+
+
